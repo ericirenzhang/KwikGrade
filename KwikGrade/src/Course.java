7e5e324c4a54585a6935187581bf785a6d59bc4e
ericirenzhang <35663825+ericirenzhang@users.noreply.github.com>
2018-11-08 23:13:01 -0500

    add back in Course.java (#5)
    Course.java was missing in PR #3 , This PR is to add it back in.

---
+++ KwikGrade/src/Course.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Course {
+	public String courseNum;
+	private String courseTerm;
+	private String courseTitle;
+	private boolean isOpen;
+
+	private ArrayList<Student> activeStudent;
+	private ArrayList<Student> inactiveStudent;
+	
+	public Course() {
+		this.courseNum = "NO NAME";
+		this.courseTerm = "NO TERM";
+		this.courseTitle = "NO TITLE";
+		this.isOpen = true;
+		this.activeStudent = new ArrayList<Student>();
+		this.inactiveStudent = new ArrayList<Student>();
+	}
+	
+	public Course(String courseNum, String courseTerm, String courseTitle) {
+		this.courseNum = courseNum;
+		this.courseTerm = courseTerm;
+		this.courseTitle = courseTitle;
+		this.isOpen = true;
+		this.activeStudent = new ArrayList<Student>();
+		this.inactiveStudent = new ArrayList<Student>();
+	}
+	
+	public void closeCourse() {
+		this.isOpen = false;
+	}
+	
+	//==========================
+	// Getters
+	//==========================
+	public String getCourseNum() {
+		return this.courseNum;
+	}
+	
+	public String getCourseTerm() {
+		return this.courseTerm;
+	}
+	
+	public String getCourseTitle() {
+		return this.courseTitle;
+	}
+	public boolean getIsOpen() {
+		return this.isOpen;
+	}
+	
+	//==========================
+	// Setters
+	//==========================
+	public void setCourseNum(String courseNum) {
+		this.courseNum = courseNum;
+	}
+	
+	public void setCourseTerm(String courseTerm) {
+		this.courseTerm = courseTerm;
+	}
+	
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+	public void setIsOpen(boolean isOpen) {
+		this.isOpen = isOpen;
+	}
+
+}
