17f1acde6577134ffac2540efc33f0b541ef7a43
Eric Iren Zhang <eric.iren.zhang@gmail.com>
2018-11-12 23:32:13 -0500

    added feature to allow changing login credentials

---
+++ KwikGrade/src/views/ChangeCreds.java
@@ -0,0 +1,113 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.awt.event.ActionEvent;
+
+public class ChangeCreds extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JPasswordField newPasswordText;
+	private JPasswordField retypePasswordText;
+	private JTextField newUsernameText;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ChangeCreds dialog = new ChangeCreds();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ChangeCreds() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		newPasswordText = new JPasswordField();
+		newPasswordText.setBounds(172, 105, 133, 20);
+		contentPanel.add(newPasswordText);
+		
+		retypePasswordText = new JPasswordField();
+		retypePasswordText.setBounds(172, 136, 133, 20);
+		contentPanel.add(retypePasswordText);
+		
+		newUsernameText = new JTextField();
+		newUsernameText.setBounds(172, 71, 133, 20);
+		contentPanel.add(newUsernameText);
+		newUsernameText.setColumns(10);
+		
+		JLabel newUsernameLabel = new JLabel("New Username");
+		newUsernameLabel.setBounds(67, 74, 95, 14);
+		contentPanel.add(newUsernameLabel);
+		
+		JLabel newPasswordLabel = new JLabel("New Password");
+		newPasswordLabel.setBounds(67, 108, 95, 14);
+		contentPanel.add(newPasswordLabel);
+		
+		JLabel retypePasswordLabel = new JLabel("Re-type Password");
+		retypePasswordLabel.setBounds(67, 139, 105, 14);
+		contentPanel.add(retypePasswordLabel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(newPasswordText.getText().equals(retypePasswordText.getText())) {
+							JOptionPane.showMessageDialog(null, "Username and Password Changed Successfully!");
+							try {
+								FileWriter changedCreds = new FileWriter("logincredentials.txt", false);
+								changedCreds.write(newUsernameText.getText()+","+newPasswordText.getText());
+								changedCreds.close();
+							}
+							catch(Exception e1) {
+								System.out.println("COULD NOT FIND FILE!!!!");
+							}
+							dispose();
+						}
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
--- KwikGrade/src/views/LoginFrame.java
+++ KwikGrade/src/views/LoginFrame.java
@@ -1,27 +1,33 @@
 package views;
 
-import java.awt.EventQueue;
-import java.awt.Image;
-
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Font;
-import javax.swing.SwingConstants;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.*;
+import java.io.File;
+import java.util.Scanner;
+import java.util.*;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class LoginFrame {
 	private static final String GRADING_IMAGE_STRING = "grading_login.jpg";
 	private JFrame frame;
 	private JTextField UID;
 	private JPasswordField PW;
+	private Scanner rawCreds;
+	//private Scanner rawCreds;
 
 	/**
 	 * Create the application.
@@ -30,9 +36,29 @@ public class LoginFrame {
 		initializeFrameContents();
 	}
 	
+	public boolean verifyCreds(String username, String password) {
+		System.getProperty("user.dir");
+		try {
+			rawCreds = new Scanner(new File("logincredentials.txt")); 
+		}
+		catch(Exception e) {
+			System.out.println("COULD NOT FIND FILE!!!!");
+		}
+		String line = rawCreds.nextLine();
+		List<String> splitLine = Arrays.asList(line.split(","));
+		if( username.equals(splitLine.get(0)) && password.equals(splitLine.get(1)) ) {
+			rawCreds.close();
+			return true;
+		}
+		else {
+			rawCreds.close();
+			return false;
+		}
+			
+	}
+	
 	public void login(String username, String password) {
-		if(username.equals("rawr") && password.equals("superman")) {
-			JOptionPane.showMessageDialog(null, "Successful Login!");
+		if(verifyCreds(username, password)) {
 			frame.dispose();
 			MainDashboard maindashboard = new MainDashboard();
 			maindashboard.setVisible(true);
@@ -41,6 +67,18 @@ public class LoginFrame {
 			JOptionPane.showMessageDialog(null, "Incorrect Login!");
 		}
 	}
+	
+	public void credChange(String username, String password) {
+		if(verifyCreds(username, password)) {
+			ChangeCreds changeCredsFrame = new ChangeCreds();
+			changeCredsFrame.setModal(true);
+			changeCredsFrame.setVisible(true);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect Login!");
+		}
+	}
+	
 
 	/**
 	 * Initialize the contents of the frame.
@@ -52,12 +90,12 @@ public class LoginFrame {
 		frame.getContentPane().setLayout(null);
 		
 		UID = new JTextField();
-		UID.setBounds(419, 121, 150, 22);
+		UID.setBounds(410, 119, 170, 22);
 		frame.getContentPane().add(UID);
 		UID.setColumns(10);
 		
 		PW = new JPasswordField();
-		PW.setBounds(419, 206, 150, 22);
+		PW.setBounds(410, 204, 170, 22);
 		frame.getContentPane().add(PW);
 		
 		PW.addKeyListener(new KeyAdapter() {
@@ -86,7 +124,7 @@ public class LoginFrame {
 			}
 		});
 		
-		loginButton.setBounds(419, 254, 150, 64);
+		loginButton.setBounds(410, 254, 170, 64);
 		frame.getContentPane().add(loginButton);
 		
 		JLabel gradePic = new JLabel("");
@@ -100,6 +138,16 @@ public class LoginFrame {
 		kwikGradeLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		kwikGradeLabel.setBounds(419, 13, 150, 38);
 		frame.getContentPane().add(kwikGradeLabel);
+		
+		JButton changeCredsButton = new JButton("Change Credentials");
+		changeCredsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				credChange(UID.getText(), PW.getText());
+			}
+		});
+		changeCredsButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		changeCredsButton.setBounds(410, 329, 170, 33);
+		frame.getContentPane().add(changeCredsButton);
 
 		frame.setVisible(true);
 	}
