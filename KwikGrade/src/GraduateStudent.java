378aad0e04785c6fb7fecd5146e030cd3afeb1b7
Nikhilesh M <nikhileshm@Nikhileshs-MacBook-Pro.local>
2018-11-16 10:36:34 -0500

    Grad and UnderGrad classes added issue #30

---
+++ KwikGrade/src/GraduateStudent.java
@@ -0,0 +1,17 @@
+
+public class GraduateStudent {
+	String status;
+
+	public GraduateStudent(String status) {
+		this.status  = status;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
---
+++ KwikGrade/src/UndergraduateStudent.java
@@ -0,0 +1,17 @@
+
+public class UndergraduateStudent {
+	String status;
+
+	public UndergraduateStudent(String status) {
+		this.status  = status;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
