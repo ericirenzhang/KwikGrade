ea2c571b5f576dc26a4ed440cef19dec746c475a
Sean <puzzledsean@gmail.com>
2018-11-19 20:09:23 -0500

    move undergraduate/graduate and add calls to createfromnewframe

--- KwikGrade/src/models/UndergraduateStudent.java
+++ KwikGrade/src/models/UndergraduateStudent.java
@@ -1,9 +1,9 @@
-import models.Student;
+package models;
 
 public class UndergraduateStudent extends Student {
 	String status;
 
-	public UndergraduateStudent(String fName, String middleInitial, String lName, String buId, String email,String status) {
+	public UndergraduateStudent(String fName, String middleInitial, String lName, String buId, String email, String status) {
 		super(fName, middleInitial, lName, buId, email);
 		this.status  = status;
 	}
